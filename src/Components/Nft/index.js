6eb6d02e080a4029506ff06f488ee353ab4917b8
Hadi6788 <89835786+Hadi6788@users.noreply.github.com>
2021-12-28 18:59:57 +0500

    create  nft

--- src/Components/Nft/index.js
+++ src/Components/Nft/index.js
@@ -1,16 +1,20 @@
 import "./style.css";
-
+import Grid from "@material-ui/core/Grid";
 const Nft = () => {
   return (
     <>
-      <div className="nft">
-        <h1>WHAT IS NFT?</h1>
-        <p>
-          A non-fungible token (NFT) is a unit of data stored on a digital
-          ledger, called a blockchain, that certifies a digital asset to be
-          unique and therefore not interchangeable.
-        </p>
-      </div>
+      <Grid container justifyContent="center" className="nft">
+        <Grid item container xs={12} justifyContent="center">
+          <h1>WHAT IS NFT?</h1>
+        </Grid>
+        <Grid item container xs={12} justifyContent="center">
+          <p>
+            A non-fungible token (NFT) is a unit of data stored on a digital
+            ledger, called a blockchain, that certifies a digital asset to be
+            unique and therefore not interchangeable.
+          </p>
+        </Grid>
+      </Grid>
     </>
   );
 };
